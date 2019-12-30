f9164a8bc58e8fc7979a3bf49b3a96fb9586c167
Sean Philippi <sean.philippi@gmail.com>
2019-12-29 18:07:12 -0600

    inputs now allowed for extentions and path

--- file-cleaner.py
+++ file-cleaner.py
@@ -3,12 +3,38 @@ import os
 
 PATH = os.environ["FILE_CLEANER_DIR_PATH"]
 
-dir = Path(PATH)
+default_dir = Path(PATH)
 
-patterns = ['*.db', '*.pdf', '*.png', '*jpg', '*jpeg']
+default_patterns = ['*.db', '*.pdf', '*.png', '*.jpg', '*.jpeg']
 
-for i in patterns:
-  files = dir.walkfiles(i)
+def file_cleaner(dir, patterns):
+  for i in patterns:
+    files = dir.walkfiles(i)
   for file in files:
     file.remove()
     print(file)
+
+print('What is your file path?')
+print('"default" for default, or enter a file path')
+answer = input()
+if answer == 'default':
+  dir = default_dir
+else:
+  dir = Path(answer)
+print('What file types do you want to delete?')
+print('"default" for default, or enter comma separated extentions to delete')
+answer2 = input()
+
+patterns = []
+
+if answer2 == 'default':
+  patterns = default_patterns
+else:
+  extension_list = answer2.split(',')
+
+  for item in extension_list:
+    item.strip(' .')
+    item = '*.' + item
+    patterns.append(item)
+
+file_cleaner(dir, patterns)
