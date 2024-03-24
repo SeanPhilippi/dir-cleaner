26afc1f06052201381cb048a45e1d4d7cf865d2d
SeanPhilippi <sean.philippi@gmail.com>
2024-03-24 13:42:57 -0400

    getting very close, using test folder, need to delete created directories if empty, and handle dupes with differing years

--- dir-cleaner.py
+++ dir-cleaner.py
@@ -3,6 +3,7 @@ from tqdm import tqdm
 import os
 import sys
 import argparse
+import re
 import shutil
 from send2trash import send2trash
 from datetime import datetime
@@ -54,7 +55,8 @@ parser.add_argument(
     "-p",
     "--path",
     help=f"Path to directory for cleaning. Default is {os.environ.get('MUSIC_PATH')}.",
-    default="MUSIC_PATH",
+    # default="MUSIC_PATH",
+    default="TEST_PATH",
 )
 
 parser.add_argument(
@@ -79,10 +81,48 @@ def dir_cleaner(dir, empty):
     cleaned_dir = Path(f"cleaned-{now.year}-{now.month}-{now.day}")
     cleaned_dir.mkdir()
 
+    # create a directory for duplicate dirs
+    dupe_dirs = cleaned_dir / "duplicate-dirs"
+    dupe_dirs.mkdir()
+
     if dir == "TRANSMISSION_DWLDS_PATH":
         patterns = transmission_dwlds_patterns
     else:
         patterns = default_patterns
+
+    # regular expression pattern for folder names to keep
+    keep = re.compile(r".*\(\d{4}\)$")
+
+    # walk through the directories and remove any that are duplicates
+    for root, dirs, files in os.walk(dir):
+        # if the folder name doesn't match the pattern, check for a duplicate
+        # sort the directories in alpha order
+        dirs.sort()
+        prev_name = None
+        for i, name in enumerate(dirs):
+            if prev_name is not None:
+                print(prev_name)
+                print(name, '\n')
+                dir_path = os.path.join(root, name)
+                prev_dir_path = os.path.join(root, prev_name)
+                dir_files = [f for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f))]
+                prev_dir_files = [f for f in os.listdir(prev_dir_path) if os.path.isfile(os.path.join(prev_dir_path, f))]
+                if prev_name.lower() == name.lower() or name.lower().startswith(prev_name.lower()) or prev_name.lower().startswith(name.lower()):
+                    # check if number of files is the same and there are no subdirectories
+                    if len(dir_files) == len(prev_dir_files) and not any(os.path.isdir(os.path.join(dir_path, f)) for f in os.listdir(dir_path)):
+                        # if both names are dupes but the years are the same
+                        # ! think I have to check that the years are not equal still
+                        if keep.match(prev_name) and keep.match(name):
+                            print("==would leave this alone 0:", prev_name, name)
+                            print(f"Error: Both '{prev_name}' and '{name}' are duplicates but the years are different")
+                        elif keep.match(prev_name):
+                            print("==would move this dupe 1:", os.path.join(root, name), "vs this:", os.path.join(root, prev_name))
+                            # shutil.move(os.path.join(root, name), dupe_dirs)
+                        elif keep.match(name):
+                            print("==would move this dupe 2:", os.path.join(root, prev_name), "vs this:", os.path.join(root, name))
+                            # shutil.move(os.path.join(root, prev_name), dupe_dirs)
+            prev_name = name
+
 	# walk files and remove files with an extension in patterns list
     for ext in tqdm(patterns):
         files = dir.walkfiles(ext)
